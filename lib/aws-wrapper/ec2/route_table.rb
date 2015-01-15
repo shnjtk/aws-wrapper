d06fe26f66480d59c90bd4eb943c3f5d1e975895
Shinji Takae <shnjtk@gmail.com>
2015-01-15 14:02:24 +0900

    add instance methods and test code

--- lib/aws-wrapper/ec2/route_table.rb
+++ lib/aws-wrapper/ec2/route_table.rb
@@ -1,8 +1,67 @@
 module AwsWrapper
   class RouteTable
+
+    # RouteTable :id or :name
+    def initialize(options)
+      @rt = RouteTable.find(options)
+      @aws_rt = AWS::EC2::RouteTable.new(@rt[:route_table_id])
+    end
+
+    def add_route(cidr, options = {})
+      @aws_rt.create_route(cidr, options)
+    end
+
+    # If cidr is nil, delete all entries(routes) in the table.
+    def delete_route(cidr = nil)
+      if cidr.nil?
+        routes = @aws_rt.routes
+        routes.each do |route|
+          @aws_rt.delete_route(route.destination_cidr_block)
+        end
+      else
+        @aws_rt.delete_route(cidr)
+      end
+    end
+
+    def routes
+      @aws_rt.routes
+    end
+
+    def has_route?(cidr)
+      routes.each do |route|
+        return true if route.destination_cidr_block == cidr
+      end
+      false
+    end
+
+    def subnets
+      @aws_rt.subnets
+    end
+
+    def associations
+      @aws_rt.associations
+    end
+
+    def associated_with?(options)
+      target_subnet = AwsWrapper::Subnet.find(options)
+      return false if target_subnet.nil?
+      subnets.each do |subnet|
+        return true if subnet.subnet_id == target_subnet[:subnet_id]
+      end
+      false
+    end
+
+    def vpc
+      @aws_rt.vpc
+    end
+
+    def delete
+      self.delete(:id => @rt[:route_table_id])
+    end
+
     class << self
       # specify vpc by :id or :name
-      def create(name, vpc, rt_entries, main = false)
+      def create(name, vpc, main = false)
         vpc = AwsWrapper::Vpc.find(vpc)
         return false if vpc.nil?
         ec2 = AWS::EC2.new
--- spec/aws_wrapper_route_table_spec.rb
+++ spec/aws_wrapper_route_table_spec.rb
@@ -5,9 +5,12 @@ module AwsWrapper
 
     VPC_NAME    = "vpc-rtable-test"
     VPC_CIDR    = "10.10.0.0/16"
+    IGW_NAME    = "igw-rtable-test"
     RTABLE_NAME = "rtable-test"
+    RTABLE_CIDR = "0.0.0.0/0"
 
     created_vpcs = []
+    created_igws = []
     created_rtables = []
 
     before(:all) do
@@ -17,8 +20,8 @@ module AwsWrapper
     end
 
     it "creates a RouteTable named \'#{RTABLE_NAME}\'" do
-      rt = AwsWrapper::RouteTable.create(RTABLE_NAME, {:name => VPC_NAME}, nil)
-      created_rtables << rt[:route_table_id]
+      rt_info = AwsWrapper::RouteTable.create(RTABLE_NAME, {:name => VPC_NAME})
+      created_rtables << rt_info[:route_table_id]
       expect(AwsWrapper::RouteTable.exists?(:name => RTABLE_NAME)).to be true
     end
 
@@ -27,9 +30,23 @@ module AwsWrapper
       expect(AwsWrapper::RouteTable.exists?(:name => RTABLE_NAME)).not_to be true
     end
 
+    it "adds an entry to the table" do
+      rt_info = AwsWrapper::RouteTable.create(RTABLE_NAME, {:name => VPC_NAME})
+      created_rtables << rt_info[:route_table_id]
+      rt = AwsWrapper::RouteTable.new(:id => rt_info[:route_table_id])
+      igw = AwsWrapper::InternetGateway.create(IGW_NAME, {:name => VPC_NAME})
+      created_igws << igw[:internet_gateway_id]
+      aws_igw = AWS::EC2::InternetGateway.new(igw[:internet_gateway_id])
+      rt.add_route(RTABLE_CIDR, {:internet_gateway => aws_igw})
+      expect(rt.has_route?(RTABLE_CIDR)).to be true
+    end
+
     after(:all) do
       created_rtables.each do |rtable_id|
         AwsWrapper::RouteTable.delete(:id => rtable_id)
+      end
+      created_igws.each do |igw_id|
+        AwsWrapper::InternetGateway.delete!(:id => igw_id)
       end
       created_vpcs.each do |vpc_id|
         AwsWrapper::Vpc.delete(:id => vpc_id)
