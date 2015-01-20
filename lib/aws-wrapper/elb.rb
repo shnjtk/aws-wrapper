95658664838227e010b0220fd72682e1e45c7165
Shinji Takae <shnjtk@gmail.com>
2015-01-20 10:12:52 +0900

    add Elb module and test code

---
+++ lib/aws-wrapper/elb.rb
@@ -0,0 +1,58 @@
+module AwsWrapper
+  class Elb
+    class << self
+      def create(name, listeners, availability_zones = [], subnets = [], security_groups = [], internal = false, tags = [])
+        options = {}
+        options[:load_balancer_name] = name
+        options[:listeners] = listeners
+        options[:availability_zones] = availability_zones unless availability_zones.empty?
+        options[:subnets] = subnets unless subnets.empty?
+        options[:security_groups] = security_groups
+        options[:schema] = "internal" if internal
+        options[:tags] = tags unless tags.empty?
+        aws_elb = AWS::ELB.new
+        aws_elb.client.create_load_balancer(options)
+      end
+
+      def delete(name)
+        aws_elb = AWS::ELB.new
+        res = aws_elb.client.delete_load_balancer(:load_balancer_name => name)
+        res[:return]
+      end
+
+      def delete!(name)
+        begin
+          delete(name)
+        rescue AWS::EC2::Errors::DependencyViolation
+          elb = find(name)
+          aws_elb = AWS::ELB.new
+          aws_elb.client.detach_load_balancer_from_subnets(:load_balancer_name => name,
+                                                           :subnets => elb.subnet_ids)
+        end
+        delete(name)
+      end
+
+      def exists?(name)
+        find(name).nil? ? false : true
+      end
+
+      def find(name)
+        aws_elb = AWS::ELB.new
+        aws_elb.load_balancers.each do |elb|
+          return elb if elb.name == name
+        end
+        nil
+      end
+
+      def create_listener(elb_protocol, elb_port, instance_protocol, instance_port, ssl_cert_id = nil)
+        listener = {}
+        listener[:protocol] = elb_protocol
+        listener[:load_balancer_port] = elb_port
+        listener[:instance_protocol] = instance_protocol
+        listener[:instance_port] = instance_port
+        listener[:ssl_certificate_id] = ssl_cert_id if ssl_cert_id
+        listener
+      end
+    end
+  end
+end
---
+++ spec/aws_wrapper_elb_spec.rb
@@ -0,0 +1,65 @@
+require "spec_helper"
+
+module AwsWrapper
+  describe Elb do
+
+    VPC_NAME = "vpc-elb-test"
+    VPC_CIDR = "10.10.0.0/16"
+    IGW_NAME = "igw-elb-test"
+    ELB_NAME = "elb-test"
+    SUBNET_NAME = "sn-vpc-test"
+    SUBNET_CIDR = "10.10.1.0/24"
+    SECURITY_GROUP_NAME = "vpc-test-security-group"
+    DESCRIPTION = "Security Group for ELB testing"
+
+    created_vpcs = []
+    created_igws = []
+    created_elbs = []
+    created_subnets = []
+    created_security_groups = []
+
+    before(:all) do
+      AwsWrapper::Core.setup
+      vpc_info = AwsWrapper::Vpc.create(VPC_NAME, VPC_CIDR)
+      created_vpcs << vpc_info[:vpc_id]
+      igw_info = AwsWrapper::InternetGateway.create(IGW_NAME, {:name => VPC_NAME})
+      created_igws << igw_info[:internet_gateway_id]
+      subnet_info = AwsWrapper::Subnet.create(SUBNET_NAME, SUBNET_CIDR, {:name => VPC_NAME})
+      created_subnets << subnet_info[:subnet_id]
+      security_group = AwsWrapper::SecurityGroup.create(SECURITY_GROUP_NAME, DESCRIPTION, {:id => vpc_info[:vpc_id]})
+      created_security_groups << security_group[:group_id]
+    end
+
+    it "creates a ELB named \'#{ELB_NAME}\'" do
+      listener = AwsWrapper::Elb.create_listener("HTTP", 80, "HTTP", 80)
+      vpc = AwsWrapper::Vpc.find(:name => VPC_NAME)
+      subnet = AwsWrapper::Subnet.find(:name => SUBNET_NAME)
+      sg = AwsWrapper::SecurityGroup.find(:name => SECURITY_GROUP_NAME)
+      elb = AwsWrapper::Elb.create(ELB_NAME, [listener], [],
+                                  [subnet[:subnet_id]], [sg[:group_id]])
+      created_elbs << ELB_NAME
+      expect(AwsWrapper::Elb.exists?(ELB_NAME)).to be true
+    end
+
+    it "deletes a ELB named \'#{ELB_NAME}\'" do
+      AwsWrapper::Elb.delete!(ELB_NAME)
+      expect(AwsWrapper::Elb.exists?(ELB_NAME)).not_to be true
+    end
+
+    after(:all) do
+      created_elbs.each do |elb_name|
+        AwsWrapper::Elb.delete!(elb_name)
+      end
+      created_subnets.each do |subnet_id|
+        AwsWrapper::Subnet.delete(:id => subnet_id)
+      end
+      created_igws.each do |igw_id|
+        AwsWrapper::InternetGateway.delete!(:id => igw_id)
+      end
+      created_vpcs.each do |vpc_id|
+        AwsWrapper::Vpc.delete(:id => vpc_id)
+      end
+    end
+
+  end
+end
