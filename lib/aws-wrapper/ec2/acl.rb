50bb96be30a1059957044d804733a1db03550cba
Shinji Takae <shnjtk@gmail.com>
2015-01-21 19:38:01 +0900

    add instance methods of Acl class and test code

--- lib/aws-wrapper/ec2/acl.rb
+++ lib/aws-wrapper/ec2/acl.rb
@@ -1,11 +1,111 @@
 module AwsWrapper
   module Ec2
     class Acl
+
+      ACTION_ALLOW = :allow
+      ACTION_DENY  = :deny
+
+      INBOUND = :ingress
+      OUTBOUND = :egress
+
+      PROTO_TCP = 6
+      PROTO_UDP = 17
+      PROTO_ALL = -1
+
+      PORT_ALL = (1..65535)
+
       def initialize(options = {})
         @acl = AwsWrapper::Ec2::Acl.find(options)
         @aws_acl = AWS::EC2::NetworkACL.new(@acl[:network_acl_id])
       end
-      attr_accessor :aws_acl
+
+      def subnets
+        @aws_acl.subnets
+      end
+
+      def inbound_rules
+        rules = []
+        @aws_acl.entries.each do |entry|
+          rules << entry unless entry.egress
+        end
+        rules
+      end
+
+      def inbound_rule_exists?(number)
+        rule_exists?(number, false)
+      end
+
+      def add_inbound_rule(number, protocol, port_range, cidr, action, options = {})
+        if rule_exists?(number, false)
+          replace_entry(number, protocol, port_range, cidr, action, false, options)
+        else
+          create_entry(number, protocol, port_range, cidr, action, false, options)
+        end
+      end
+
+      def delete_inbound_rule(number)
+        delete_entry(:ingress, number)
+      end
+
+      def outbound_rules
+        rules = []
+        @aws_acl.entries.each do |entry|
+          rules << entry if entry.egress
+        end
+        rules
+      end
+
+      def outbound_rule_exists?(number)
+        rule_exists?(number, true)
+      end
+
+      def add_outbound_rule(number, protocol, port_range, cidr, action, options = {})
+        if rule_exists?(number, true)
+          replace_entry(number, protocol, port_range, cidr, action, true, options)
+        else
+          create_entry(number, protocol, port_range, cidr, action, true, options)
+        end
+      end
+
+      def delete_outbound_rule(number)
+        delete_entry(:egress, number)
+      end
+
+      def create_entry(number, protocol, port_range, cidr, action, egress, options = {})
+        options[:rule_number] = number
+        options[:action] = action
+        options[:protocol] = protocol
+        options[:cidr_block] = cidr
+        options[:egress] = egress
+        options[:port_range] = port_range
+        @aws_acl.create_entry(options)
+      end
+      private :create_entry
+
+      def delete_entry(egress_or_ingress, number)
+        @aws_acl.delete_entry(egress_or_ingress, number)
+      end
+      private :delete_entry
+
+      def replace_entry(number, protocol, port_range, cidr, action, egress, options = {})
+        options[:rule_number] = number
+        options[:action] = action
+        options[:protocol] = protocol
+        options[:cidr_block] = cidr
+        options[:egress] = egress
+        options[:port_range] = port_range
+        @aws_acl.replace_entry(options)
+      end
+      private :replace_entry
+
+      def rule_exists?(number, egress = false)
+        rules = egress ? outbound_rules : inbound_rules
+        rules.each do |rule|
+          return true if rule.rule_number == number
+        end
+        false
+      end
+      private :rule_exists?
 
       class << self
         def create(name, vpc = {}, options = {})
--- spec/aws_wrapper_acl.rb
+++ spec/aws_wrapper_acl.rb
@@ -3,9 +3,22 @@ require "spec_helper"
 module AwsWrapper
   module Ec2
     describe Acl do
+
       VPC_NAME = "vpc-acl-test"
       VPC_CIDR = "10.10.0.0/16"
       ACL_NAME = "acl-test"
+
+      INBOUND_NUMBER = 100
+      INBOUND_PROTO  = AwsWrapper::Ec2::Acl::PROTO_TCP
+      INBOUND_PORT   = AwsWrapper::Ec2::Acl::PORT_ALL
+      INBOUND_CIDR   = "0.0.0.0/0"
+      INBOUND_ACTION = AwsWrapper::Ec2::Acl::ACTION_ALLOW
+
+      OUTBOUND_NUMBER = 100
+      OUTBOUND_PROTO  = AwsWrapper::Ec2::Acl::PROTO_TCP
+      OUTBOUND_PORT   = AwsWrapper::Ec2::Acl::PORT_ALL
+      OUTBOUND_CIDR   = "0.0.0.0/0"
+      OUTBOUND_ACTION = AwsWrapper::Ec2::Acl::ACTION_ALLOW
 
       created_vpcs = []
       created_acls = []
@@ -20,6 +33,32 @@ module AwsWrapper
         acl = AwsWrapper::Ec2::Acl.create(ACL_NAME, {:name => VPC_NAME})
         created_acls << acl[:network_acl_id]
         expect(AwsWrapper::Ec2::Acl.exists?(:name => ACL_NAME)).to be true
+      end
+
+      it "adds the inbound rule" do
+        acl = AwsWrapper::Ec2::Acl.new(:name => ACL_NAME)
+        acl.add_inbound_rule(INBOUND_NUMBER, INBOUND_PROTO, INBOUND_PORT,
+                             INBOUND_CIDR, INBOUND_ACTION)
+        expect(acl.inbound_rule_exists?(INBOUND_NUMBER)).to be true
+      end
+
+      it "deletes the inbound rule" do
+        acl = AwsWrapper::Ec2::Acl.new(:name => ACL_NAME)
+        acl.delete_inbound_rule(INBOUND_NUMBER)
+        expect(acl.inbound_rule_exists?(INBOUND_NUMBER)).not_to be true
+      end
+
+      it "adds the outbound rule" do
+        acl = AwsWrapper::Ec2::Acl.new(:name => ACL_NAME)
+        acl.add_outbound_rule(OUTBOUND_NUMBER, OUTBOUND_PROTO, INBOUND_PORT,
+                              OUTBOUND_CIDR, OUTBOUND_ACTION)
+        expect(acl.outbound_rule_exists?(OUTBOUND_NUMBER)).to be true
+      end
+
+      it "deletes the outbound rule" do
+        acl = AwsWrapper::Ec2::Acl.new(:name => ACL_NAME)
+        acl.delete_outbound_rule(OUTBOUND_NUMBER)
+        expect(acl.outbound_rule_exists?(OUTBOUND_NUMBER)).not_to be true
       end
 
       it "deletes Network ACL named '#{ACL_NAME}'" do
