d5278dc6a4564a403e240196c65c74e616535fcf
Shinji Takae <shnjtk@gmail.com>
2015-02-03 10:38:52 +0900

    add test code for delete! method testing

--- spec/aws_wrapper_security_group_spec.rb
+++ spec/aws_wrapper_security_group_spec.rb
@@ -6,6 +6,9 @@ module AwsWrapper
 
       VPC_NAME             = "vpc-securigy-group-test"
       VPC_CIDR             = "10.10.0.0/16"
+      SUBNET_NAME          = "sn-security-group-test"
+      SUBNET_CIDR          = "10.10.0.0/24"
+      INTERFACE_NAME       = "eni-security-group-test"
       SECURITY_GROUP_NAME  = "vpc-security-group-test-sg"
       DESCRIPTION          = "Security Group for testing"
       INBOUND_PROTO        = AwsWrapper::Ec2::SecurityGroup::PROTO_TCP
@@ -14,14 +17,23 @@ module AwsWrapper
       OUTBOUND_PROTO       = AwsWrapper::Ec2::SecurityGroup::PROTO_ALL
       OUTBOUND_PORT        = AwsWrapper::Ec2::SecurityGroup::PORT_ALL
       OUTBOUND_DESTINATION = "0.0.0.0/0"
+      INBOUND_PROTO_2      = AwsWrapper::Ec2::SecurityGroup::PROTO_ALL
+      INBOUND_PORT_2       = AwsWrapper::Ec2::SecurityGroup::PORT_ALL
+      INBOUND_SOURCE_2     = "0.0.0.0/0"
 
       created_vpcs = []
+      created_subnets = []
+      created_interfaces = []
       created_security_groups = []
 
       before(:all) do
         AwsWrapper::Core.setup
         vpc_info = AwsWrapper::Ec2::Vpc.create(VPC_NAME, VPC_CIDR)
         created_vpcs << vpc_info[:vpc_id]
+        subnet = AwsWrapper::Ec2::Subnet.create(SUBNET_NAME, SUBNET_CIDR, VPC_NAME)
+        created_subnets << subnet[:subnet_id]
+        interface = AwsWrapper::Ec2::NetworkInterface.create(INTERFACE_NAME, SUBNET_NAME)
+        created_interfaces << interface[:network_interface_id]
       end
 
       it "creates a SecurityGroup named \'#{SECURITY_GROUP_NAME}\'" do
@@ -64,9 +76,45 @@ module AwsWrapper
         expect(AwsWrapper::Ec2::SecurityGroup.exists?(SECURITY_GROUP_NAME)).not_to be true
       end
 
+      it "add inbound rule referencing itself" do
+        sg_info = AwsWrapper::Ec2::SecurityGroup.create(
+          SECURITY_GROUP_NAME, DESCRIPTION, VPC_NAME
+        )
+        created_security_groups << sg_info[:group_id]
+        sg = AwsWrapper::Ec2::SecurityGroup.new(SECURITY_GROUP_NAME)
+        sg.add_inbound_rule(INBOUND_PROTO_2, INBOUND_PORT_2, INBOUND_SOURCE_2)
+        expect(sg.has_inbound_rule?(INBOUND_PROTO_2, INBOUND_PORT_2, INBOUND_SOURCE_2)).to be true
+      end
+
+      it "deletes SecurityGroup that has a rule referencing itself" do
+        AwsWrapper::Ec2::SecurityGroup.delete!(SECURITY_GROUP_NAME)
+        expect(AwsWrapper::Ec2::SecurityGroup.exists?(SECURITY_GROUP_NAME)).not_to be true
+      end
+
+      it "associates with network interface" do
+        sg_info = AwsWrapper::Ec2::SecurityGroup.create(
+          SECURITY_GROUP_NAME, DESCRIPTION, VPC_NAME
+        )
+        created_security_groups << sg_info[:group_id]
+        eni = AwsWrapper::Ec2::NetworkInterface.new(INTERFACE_NAME)
+        eni.add_security_group(SECURITY_GROUP_NAME)
+        expect(eni.has_security_group?(SECURITY_GROUP_NAME)).to be true
+      end
+
+      it "deletes SecurityGroup that associates with network interface" do
+        AwsWrapper::Ec2::SecurityGroup.delete!(SECURITY_GROUP_NAME)
+        expect(AwsWrapper::Ec2::SecurityGroup.exists?(SECURITY_GROUP_NAME)).not_to be true
+      end
+
       after(:all) do
         created_security_groups.each do |sg_id|
-          AwsWrapper::Ec2::SecurityGroup.delete(sg_id)
+          AwsWrapper::Ec2::SecurityGroup.delete!(sg_id)
+        end
+        created_interfaces.each do |interface_id|
+          AwsWrapper::Ec2::NetworkInterface.delete(interface_id)
+        end
+        created_subnets.each do |subnet_id|
+          AwsWrapper::Ec2::Subnet.delete!(subnet_id)
         end
         created_vpcs.each do |vpc_id|
           AwsWrapper::Ec2::Vpc.delete(vpc_id)
